303ed9f4e72307926c4321dee10f32b80971177f
Rob Coffman <rcoffman@redhat.com>
2017-01-12 08:29:58 -0700

    adding root domain for cloudflare

--- terraform/infrastructure/dns.tf
+++ terraform/infrastructure/dns.tf
@@ -4,7 +4,17 @@ provider "cloudflare" {
     token = "${var.cloudflare_token}"
 }
 
-# Add a record to the domain
+# Add root canme (flattened) record
+resource "cloudflare_record" "root" {
+    domain = "blackestsin.com"
+    name = "@"
+    value = "copperleaf-blackestsin.s3-website-us-west-2.amazonaws.com"
+    type = "CNAME"
+    ttl = 3600
+    proxied = "false"
+}
+
+# Add www record
 resource "cloudflare_record" "www" {
     domain = "blackestsin.com"
     name = "www"
