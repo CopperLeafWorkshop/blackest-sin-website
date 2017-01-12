bd2416a9f62601cdad4c54f85dbb08a810741265
Rob Coffman <rcoffman@redhat.com>
2017-01-12 09:54:36 -0700

    adding redirect for root domain

--- terraform/infrastructure/s3.tf
+++ terraform/infrastructure/s3.tf
@@ -4,12 +4,21 @@ provider "aws" {
   region     = "us-west-2"
 }
 
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "site" {
     bucket = "copperleaf-blackestsin"
     acl = "public-read"
 
     website {
         index_document = "index.html"
         error_document = "error.html"
+    }
+}
+
+resource "aws_s3_bucket" "redirect" {
+    bucket = "copperleaf-blackestsin-redirect"
+    acl = "public-read"
+
+    website {
+        redirect_all_requests_to = "http://www.blackestsin.com/"
     }
 }
